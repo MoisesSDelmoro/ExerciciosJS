8c49708a8e496c94bbee89ce5b8bb217cbae77db
MoisesSDelmoro <57488202+MoisesSDelmoro@users.noreply.github.com>
2021-03-19 12:24:24 -0300

    v1.2

---
+++ array/forEach2.js
@@ -0,0 +1,17 @@
+Array.prototype.forEach2 = function(callback){
+    for(let i=0; i < this.length; i++){
+        callback(this[i], i, this)
+    }
+}
+
+const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
+
+aprovados.forEach2(function(nome, indice, array){
+    console.log(`${indice + 1} ${nome}`)
+    console.log(array)
+})
+
+aprovados.forEach(nome => console.log(nome));
+
+const exibirAprovados = aprovado => console.log(aprovado)
+aprovados.forEach(exibirAprovados)
